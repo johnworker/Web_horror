59bf626162121bcd8d83e9761c6d65e5f04d9454
johnworker <besscomdark@gmail.com>
2021-05-02 07:50:34 +0800

    添加內容

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// 卡片效果
+// GSAP
+gsap.to(".fly", {
+    scrollTrigger: {
+        tigger: ".fly",
+        toggleActions: "play reset none play",
+        start: "35% 50%",
+        end: "45% 20%",
+        scrub: 1,
+        markers: true
+    },
+    x: -350,
+    duration: 3
+});
