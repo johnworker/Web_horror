079942db20e74b5f0fd6d56a9f84eb805cdad3ff
johnworker <besscomdark@gmail.com>
2021-05-08 11:31:30 +0800

    樣式更新

--- main.js
+++ main.js
@@ -3,6 +3,22 @@ $(".menu-btn").click(function(){
     $(".menu-wrap").toggleClass("active");
     $(this).toggleClass("active");
 });
+// 下拉選單
+$(function() {
+  
+    // Dropdown toggle
+    $('.dropdown-toggle').click(function(){
+      $(this).next('.dropdown').toggle();
+    });
+    
+    $(document).click(function(e) {
+      var target = e.target;
+      if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+        $('.dropdown').hide();
+      }
+    });
+    
+    });    
 // GSAP
 // Ghost
 gsap.to(".fly", {
