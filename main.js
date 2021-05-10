ebdf28469a3de51367eb30f1285fb73b84ae6014
johnworker <besscomdark@gmail.com>
2021-05-11 07:28:53 +0800

    修改樣式

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
 // 選單效果
-$(".menu-btn").click(function(){
-    $(".menu-wrap").toggleClass("active");
-    $(this).toggleClass("active");
+$(".menu-btn").click(function () {
+  $(".menu-wrap").toggleClass("active");
+  $(this).toggleClass("active");
 });
 // 下拉選單
-$(function() {
-  
-    // Dropdown toggle
-    $('.dropdown-toggle').click(function(){
-      $(this).next('.dropdown').toggle();
-    });
-    
-    $(document).click(function(e) {
-      var target = e.target;
-      if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
-        $('.dropdown').hide();
-      }
-    });
-    
-    }); 
-//輪播調整  
-$(".carousel").carousel({
-  interval: 3000,
-  pause: false
+$(function () {
+
+  // Dropdown toggle
+  $('.dropdown-toggle').click(function () {
+    $(this).next('.dropdown').toggle();
+  });
+
+  $(document).click(function (e) {
+    var target = e.target;
+    if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+      $('.dropdown').hide();
+    }
+  });
+
 });
+//輪播調整  
 // GSAP
 // Ghost
 gsap.to(".fly", {
-    scrollTrigger: {
-        tigger: ".fly",
-        toggleActions: "restart none none none",
-        start: "35% 50%",
-        end: "45% 20%",
-        // scrub: 1,
-        // markers: true
-    },
+  scrollTrigger: {
+    tigger: ".fly",
+    toggleActions: "restart none none none",
+    start: "35% 50%",
+    end: "45% 20%",
+    // scrub: 1,
+    // markers: true
+  },
 
-    x: -700,
-    y: -100,
-    duration: 3
-    }
+  x: -700,
+  y: -100,
+  duration: 3
+}
 
 );
